b85306d91836f6a4535486add3feb2068b308060
Mike Glyc <mikeglyc@Mikes-MacBook-Pro.local>
2023-03-08 12:40:37 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import logo from './logo.svg';
+import { Global, css } from '@emotion/react'
 import './App.css';
+import { createGlobalStyle } from 'styled-components';
+ 
+const GlobalStyle = createGlobalStyle`
+  p {
+    color: orange;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+      <Global
+        styles={css`
+          .some-class {
+            color: hotpink !important;
+          }
+        `}
+      />
+      <Global
+        styles={css`
+          .some-class {
+            color: red !important;
+          }
+        `}
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
